00fc7b0df9dffb0a6409a4e0fc42971901c701f6
Son Tran <tran.tro@ascendcorp.com>
2019-05-08 22:38:54 +0700

    [hello world] 2nd commit
    - constant
    - subtests, testing Helper()
    - if statement

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Son Tran")
-	want := "Hello, Son Tran"
-
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
 	}
+
+	t.Run("saying hello wold to people", func(t *testing.T) {
+		got := Hello("Son Tran")
+		want := "Hello, Son Tran"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
