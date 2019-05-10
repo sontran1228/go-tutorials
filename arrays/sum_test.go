f566e274ae93ebef3997af5ebcb1d6491c8d2bf1
Son Tran <tran.tro@ascendcorp.com>
2019-05-10 21:03:09 +0700

    [arrays and slices] 3rd commit
    - slices

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -4,12 +4,33 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+	assertResult := func(t *testing.T, numbers []int, got, want int) {
 
-	got := Sum(numbers)
-	want := 15
+		t.Helper()
 
-	if want != got {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+
 	}
+
+	t.Run("Collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		assertResult(t, numbers, got, want)
+
+	})
+
+	t.Run("Collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4}
+
+		got := Sum(numbers)
+		want := 10
+
+		assertResult(t, numbers, got, want)
+	})
+
 }
