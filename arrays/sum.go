7f3c00035f2ddeb5c4cc3e2149b46760c0df986d
Son Tran <tran.tro@ascendcorp.com>
2019-05-10 20:51:05 +0700

    [arrays and slices] 2nd commit
    - keyword: range
    - blank identifier: _

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,12 @@
 package arrays
 
 func Sum(numbers [5]int) int {
+
 	sum := 0
-	for i := 0; i < len(numbers); i++ {
-		sum += numbers[i]
+
+	for _, number := range numbers {
+		sum += number
 	}
+
 	return sum
 }
