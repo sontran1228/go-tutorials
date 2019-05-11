4737cab976a0a83d8713e3791d5c1020a13f326a
Son Tran <tran.tro@ascendcorp.com>
2019-05-11 21:55:48 +0700

    [pointers-errors] 2nd commit
    - making new types: syntax “type NewTypeName OriginalType”
    - String formater

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -2,18 +2,32 @@ package pointers
 
 import "fmt"
 
+// Stringer is used to format the data to be a string
+type Stringer interface {
+	String() string
+}
+
+// Bitcoin is a declaration of new type from existing ones which is "int"
+type Bitcoin int
+
+// by making a new type, we can declare methods on them
+// to check the result of this formater method, change the test case to make it failed.
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 // Wallet has balance information of a wallet
 type Wallet struct {
 	// Spec: In Go if a symbol (so variables, types, functions et al) starts with a lowercase symbol then it is private outside the package it's defined in.
 	// balance is package private because it starts with lowercase symbol "b"
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
