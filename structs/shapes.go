863fdadcc4270ca9cee30a5b3c2be5602f24fdf0
Son Tran <tran.tro@ascendcorp.com>
2019-05-11 11:00:57 +0700

    [structs-methods-interfaces] 2nd commit
    - methods and interfaces

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,18 +1,28 @@
 package shapes
 
-type Rectangle struct {
-	width  float64
-	height float64
-}
-
-// type Circle struct {
-// 	radius float64
-// }
+import "math"
 
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.width + rectangle.height)
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.width * rectangle.height
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
 }
