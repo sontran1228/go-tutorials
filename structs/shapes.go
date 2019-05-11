1d7acbff805cb8d55f1c769763ad0df28af74c0b
Son Tran <tran.tro@ascendcorp.com>
2019-05-11 10:12:12 +0700

    [structs-methods-interfaces] 1st commit
    - structs

---
+++ structs/shapes.go
@@ -0,0 +1,18 @@
+package shapes
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+// type Circle struct {
+// 	radius float64
+// }
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.width + rectangle.height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.width * rectangle.height
+}
