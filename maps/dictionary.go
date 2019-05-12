8edd110f5231b9747019a1a689a4155a03e8dbd1
Son Tran <tran.tro@ascendcorp.com>
2019-05-12 11:15:46 +0700

    [maps] 2nd commit
    - Constant errors: https://dave.cheney.net/2016/04/07/constant-errors

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,11 +1,34 @@
 package maps
 
-import "errors"
-
 // Dictionary is a map to store key:value
+// - Maps is reference type therefore we can modify them without passing them as a pointer
+// - Becuase maps is reference type, so maps can be a nil value:
+//     + A nil map behaves like an empty map when reading,
+//     + BUT attempts to write to a nil map will cause a runtime panic
+// - Therefore, you should never initialize an empty map variable
+//     + var m map[string]string               (X)
+//     + dictionary = map[string]string{}      (V)
+//     + dictionary = make(map[string]string)  (V)
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+// DictionaryErr is used to hold error message
+type DictionaryErr string
+
+const (
+	// ErrNotFound is definition of an error in case the word couldn't be found in the dictionary
+	ErrNotFound = DictionaryErr("could not find the word you were looking for")
+
+	// ErrWordExists is definition of an error in case the word have been already existing in the dictionary
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+
+	// ErrWordDoesNotExist is definition of an error in case the word haven't been existing in the dictionary
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+// implemented error interface
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 // Search is used to find a value from a "word"
 func (d Dictionary) Search(word string) (string, error) {
@@ -14,4 +37,37 @@ func (d Dictionary) Search(word string) (string, error) {
 		return "", ErrNotFound
 	}
 	return definition, nil
+}
+
+// Add is used to put a pair of word:definition to the dictionary
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+// Update is used to update the existing definition
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+	return nil
+}
+
+// Delete is used to delete a definition
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -7,21 +7,76 @@ func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
 	t.Run("know word", func(t *testing.T) {
-
 		got, _ := dictionary.Search("test")
 		want := "this is just a test"
 
 		assertString(t, got, want)
-
 	})
 
 	t.Run("unknow word", func(t *testing.T) {
-
 		_, got := dictionary.Search("unknown")
 
 		assertError(t, got, ErrNotFound)
-
 	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new definition"
+
+		err := dictionary.Update(word, newDefinition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	word := "test"
+	dictionary := Dictionary{word: "test definition"}
+
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+	if err != ErrNotFound {
+		t.Errorf("Expected '%s' to be deleted", word)
+	}
 }
 
 func assertString(t *testing.T, got, want string) {
@@ -37,5 +92,18 @@ func assertError(t *testing.T, got, want error) {
 
 	if got != want {
 		t.Errorf("got error '%s' want '%s'", got, want)
+	}
+}
+
+func assertDefinition(t *testing.T, d Dictionary, word, definition string) {
+
+	got, err := d.Search("test")
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if got != definition {
+		t.Errorf("got '%s' want '%s'", got, definition)
 	}
 }
