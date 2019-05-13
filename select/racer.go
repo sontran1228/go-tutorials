c0ce913a77e651bea9ad24146ebf03f8f5d954e5
Son Tran <tran.tro@ascendcorp.com>
2019-05-13 19:59:25 +0700

    [select] 4th commit
    add timeout

--- select/racer.go
+++ select/racer.go
@@ -1,19 +1,29 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-// Racer compares the response times of a and b, returning the fastest one
-func Racer(a, b string) (winner string) {
+var tenSecondTimeout = 10 * time.Second
+
+// Racer compares the response times of a and b, returning the fastest one, timing out after 10s
+func Racer(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+// ConfigurableRacer compares the response times of a and b, returning the fastest one
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
 	// "myVar := <-ch": This is a blocking call, as you're waiting for a value.
 	// What "select" lets you do is wait on multiple channels. The first one to send a value "wins" and the code underneath the case is executed.
 	select {
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
 
@@ -24,10 +34,4 @@ func ping(url string) chan bool {
 		ch <- true
 	}()
 	return ch
-}
-
-func measureResponseTime(url string) time.Duration {
-	startA := time.Now()
-	http.Get(url)
-	return time.Since(startA)
 }
