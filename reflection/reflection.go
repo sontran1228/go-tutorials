70818ab7c14d1f98dfdc1eac61d6b26fba41d2e4
Son Tran <tran.tro@ascendcorp.com>
2019-05-13 22:40:46 +0700

    [reflection] 8th commit
    - reflection on map type

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -9,14 +9,18 @@ func walk(x interface{}, fn func(input string)) {
 	var getField func(int) reflect.Value
 
 	switch val.Kind() {
-	case reflect.Slice:
-		numberOfValues = val.Len()
-		getField = val.Index
+	case reflect.String:
+		fn(val.String())
 	case reflect.Struct:
 		numberOfValues = val.NumField()
 		getField = val.Field
-	case reflect.String:
-		fn(val.String())
+	case reflect.Slice, reflect.Array:
+		numberOfValues = val.Len()
+		getField = val.Index
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walk(val.MapIndex(key).Interface(), fn)
+		}
 	}
 
 	for i := 0; i < numberOfValues; i++ {
--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -13,13 +13,61 @@ func TestWalk(t *testing.T) {
 		ExpectedCalls []string
 	}{
 		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		}, {
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		}, {
+			"Struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		}, {
+			"Nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		}, {
+			"Pointers to things",
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		}, {
 			"Slices",
 			[]Profile{
 				{33, "London"},
 				{34, "Reykjavík"},
 			},
 			[]string{"London", "Reykjavík"},
-		},
+		}, {
+			"Arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		}, /*{
+			"Maps",
+			map[string]string{
+				"Foo": "Bar",
+				"Baz": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},*/
 	}
 
 	for _, test := range cases {
@@ -33,6 +81,35 @@ func TestWalk(t *testing.T) {
 		}
 	}
 
+	// Go do not guarantee order. So the tests will sometimes fail because the assertion that the calls to fn are done in a particular order.
+	// To fix this, we'll need to move our assertion with the maps to a new test where we do not care about the order.
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %+v to contain '%s' but it didnt", haystack, needle)
+	}
 }
 
 type Person struct {
