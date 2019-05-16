fc8a1bf4bbccfd2f0ac555ecf2bf78d1eea8f57b
Son Tran <tran.tro@ascendcorp.com>
2019-05-16 11:14:43 +0700

    [json] 1st commit
    - route: http.NewServeMux()

---
+++ json/InMemoryPlayerStore.go
@@ -0,0 +1,21 @@
+package main
+
+// NewInMemoryPlayerStore init NewInMemoryPlayerStore
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{map[string]int{}}
+}
+
+// InMemoryPlayerStore is an implementation of PlayerStore to hold player information in Memory
+type InMemoryPlayerStore struct {
+	store map[string]int
+}
+
+// GetPlayerScore receive player name, then return player score
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return i.store[name]
+}
+
+// RecordWin record the win player
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.store[name]++
+}
---
+++ json/server.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// PlayerStore is an interface that acts as repository to store player score
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+	RecordWin(name string)
+}
+
+// PlayerServer is a HTTP interface for player information
+// It will an implementation of "http.Handler" interface by implementing function "ServeHTTP"
+// see more: server.go -> type Handler interface
+type PlayerServer struct {
+	store PlayerStore
+}
+
+// PlayerServer return player information.
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	router := http.NewServeMux()
+
+	router.Handle("/league", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	router.Handle("/players/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		player := r.URL.Path[len("/players/"):]
+		switch r.Method {
+		case http.MethodPost:
+			p.processWin(w, player)
+		case http.MethodGet:
+			p.showScore(w, player)
+		}
+	}))
+
+	router.ServeHTTP(w, r)
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
+
+	score := p.store.GetPlayerScore(player)
+
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+
+	p.store.RecordWin(player)
+
+	w.WriteHeader(http.StatusAccepted)
+}
