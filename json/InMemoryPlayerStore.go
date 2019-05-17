e14091e91015b1459ff71b990bb9a5185315c4d5
Son Tran <tran.tro@ascendcorp.com>
2019-05-17 13:00:48 +0700

    [json] 8th commit
    refactor InMemoryPlayerStore

--- json/InMemoryPlayerStore.go
+++ json/InMemoryPlayerStore.go
@@ -20,6 +20,12 @@ func (i *InMemoryPlayerStore) RecordWin(name string) {
 	i.store[name]++
 }
 
+// GetLeague return all players information
 func (i *InMemoryPlayerStore) GetLeague() []Player {
-	return nil
+	var league []Player
+
+	for name, wins := range i.store {
+		league = append(league, Player{name, wins})
+	}
+	return league
 }
--- json/server_integration_test.go
+++ json/server_integration_test.go
@@ -14,11 +14,23 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
 
-	response := httptest.NewRecorder()
+	t.Run("get score", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newGetScoreRequest(player))
+		assertStatus(t, response.Code, http.StatusOK)
 
-	server.ServeHTTP(response, newGetScoreRequest(player))
+		assertResponseBody(t, response.Body.String(), "3")
+	})
 
-	assertStatus(t, response.Code, http.StatusOK)
+	t.Run("get league", func(t *testing.T) {
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, newLeagueRequest())
+		assertStatus(t, response.Code, http.StatusOK)
 
-	assertResponseBody(t, response.Body.String(), "3")
+		got := getLeagueFromResponse(t, response.Body)
+		want := []Player{
+			{"Pepper", 3},
+		}
+		assertLeague(t, got, want)
+	})
 }
