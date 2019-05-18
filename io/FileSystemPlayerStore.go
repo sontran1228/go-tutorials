a042e19580483a74a0e2fda2c0298ea345893ff0
Son Tran <tran.tro@ascendcorp.com>
2019-05-18 15:30:35 +0700

    [io] 2nd commit
    - change Reader to ReadSeeker: https://quii.gitbook.io/learn-go-with-tests/build-an-application/io#seeking-problems

--- io/FileSystemPlayerStore.go
+++ io/FileSystemPlayerStore.go
@@ -6,11 +6,12 @@ import (
 
 // FileSystemPlayerStore is to store data in file
 type FileSystemPlayerStore struct {
-	database io.Reader
+	database io.ReadSeeker
 }
 
 // GetLeague return all players information
 func (s *FileSystemPlayerStore) GetLeague() []Player {
+	s.database.Seek(0, 0)
 	league, _ := NewLeague(s.database)
 	return league
 }
